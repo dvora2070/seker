13da663d3f6925b2b9b25bb71ad46d3f83be1780
ayalay062 <ayalay062@gmail.com>
2021-06-15 22:35:08 +0300

    Ayala

--- project1/src/app/app.component.ts
+++ project1/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from "@angular/core";
 import { Component } from "@angular/core";
 import { UserService } from "./user.service";
 
@@ -6,11 +7,17 @@ import { UserService } from "./user.service";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   userLogin = false;
   constructor(private userService: UserService) {
     this.userService.loginUser.subscribe((x) => {
       this.userLogin = true;
     });
+  }
+  ngOnInit() {
+    var userLogin = localStorage.getItem("user");
+    if (userLogin !== null) {
+      this.userLogin = true;
+    }
   }
 }
